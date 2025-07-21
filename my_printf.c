fd87e5dad246e2a89109f8a9c1e7ad793c385587
Nyratech678 <uwuchocolatemilk09876@proton.me>
2025-07-21 14:17:11 +0200

    Add configuration for VSCode and improve _printf function implementation

--- my_printf.c
+++ my_printf.c
@@ -5,17 +5,36 @@
 #include "main.h"
 
 /**
- * _printf - Custom printf function
- * @format: Format string
- *
- * Return: Number of characters printed
- */
-
+* _printf - Custom printf function
+* @format: Format string
+*
+* Return: Number of characters printed
+*/
 
 int _printf(const char *format, ...)
 {
-    va_list args;
-    
-}
+	va_list args;
+	int i = 0;
 
- 
+	int count = 0;
+
+	if (format == NULL)
+		return (-1);
+	va_start(args, format);
+	while (format[i] != '\0')
+	{
+		if (format[i] != '%')
+		{
+			write(1, &format[i], 1);
+			count++;
+			i++;
+		}
+		else
+		{
+			i++;
+			count += handle_format(format, &i, args);
+		}
+	}
+	va_end(args);
+	return (count);
+}
