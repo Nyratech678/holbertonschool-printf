a1b7d8215a7bbc1acd8bb3f095a23a8d6e58271b
Nyratech678 <uwuchocolatemilk09876@proton.me>
2025-07-21 13:25:46 +0200

    Implement initial structure for custom _printf function

---
+++ my_printf.c
@@ -0,0 +1,21 @@
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include "main.h"
+
+/**
+ * _printf - Custom printf function
+ * @format: Format string
+ *
+ * Return: Number of characters printed
+ */
+
+
+int _printf(const char *format, ...)
+{
+    va_list args;
+    
+}
+
+ 
